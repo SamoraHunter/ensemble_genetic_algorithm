295f6a6665b6aebc50e0ba88c7487b4d5c0f12f1
SamoraHunter <samorahunter@hotmail.co.uk>
2024-04-24 13:05:07 +0100

    updated example usage

--- ml_grid/pipeline/read_in.py
+++ ml_grid/pipeline/read_in.py
@@ -11,10 +11,14 @@ class read:
 
         self.raw_input_data = pd.read_csv(filename)
 
+
 import pandas as pd
 
+
 class read_sample:
-    def __init__(self, input_filename: str, test_sample_n: int, column_sample_n: int) -> None:
+    def __init__(
+        self, input_filename: str, test_sample_n: int, column_sample_n: int
+    ) -> None:
         """
         Initialize the class with the input filename, test sample number, and column sample number.
 
@@ -25,16 +29,25 @@ class read_sample:
         """
         self.filename = input_filename
 
-        necessary_columns = ['outcome_var_1', 'age', 'male']
+        necessary_columns = ["outcome_var_1", "age", "male"]
 
         print(f"Init main > read_sample on {self.filename}")
 
         if test_sample_n == 0 and column_sample_n == 0:
-            self.raw_input_data = None  # Set raw_input_data to None if both parameters are 0
+            self.raw_input_data = (
+                None  # Set raw_input_data to None if both parameters are 0
+            )
         elif test_sample_n == 0:
-            self.raw_input_data = pd.read_csv(self.filename, usecols=lambda x: x in necessary_columns)  # Read only specified columns
+            self.raw_input_data = pd.read_csv(
+                self.filename, usecols=lambda x: x in necessary_columns
+            )  # Read only specified columns
         elif column_sample_n == 0:
-            self.raw_input_data = pd.read_csv(self.filename, nrows=test_sample_n)  # Read only specified rows
+            self.raw_input_data = pd.read_csv(
+                self.filename, nrows=test_sample_n
+            )  # Read only specified rows
         else:
-            self.raw_input_data = pd.read_csv(self.filename, nrows=test_sample_n, usecols=lambda x: x in necessary_columns)  # Read both rows and columns
-
+            self.raw_input_data = pd.read_csv(
+                self.filename,
+                nrows=test_sample_n,
+                usecols=lambda x: x in necessary_columns,
+            )  # Read both rows and columns
