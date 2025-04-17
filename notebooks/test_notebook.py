3c7c88671a1a3dd129b346bb47b2a7056f3d51a4
SamoraHunter <44898312+SamoraHunter@users.noreply.github.com>
2025-04-17 09:37:01 +0100

    Create test_notebook.py
    for git hook push protection, test main program run on test data

---
+++ notebooks/test_notebook.py
@@ -0,0 +1,11 @@
+import pytest
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+
+def test_notebook():
+    with open("notebooks/example_usage.ipynb") as f:
+        nb = nbformat.read(f, as_version=4)
+
+    ep = ExecutePreprocessor(timeout=600, kernel_name="ga_env")
+    ep.preprocess(nb)  # Will raise an error if the notebook fails
