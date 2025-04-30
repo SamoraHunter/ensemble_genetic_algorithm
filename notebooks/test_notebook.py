222c27803e9bd67e0ba62d30ea4e80fedbbe1840
SamoraHunter <samorahunter@hotmail.co.uk>
2025-04-30 17:46:04 +0100

    increased notebook test timeout

--- notebooks/test_notebook.py
+++ notebooks/test_notebook.py
@@ -7,5 +7,5 @@ def test_notebook():
     with open("notebooks/example_usage.ipynb") as f:
         nb = nbformat.read(f, as_version=4)
 
-    ep = ExecutePreprocessor(timeout=600, kernel_name="ga_env")
+    ep = ExecutePreprocessor(timeout=2400, kernel_name="ga_env")
     ep.preprocess(nb)  # Will raise an error if the notebook fails
