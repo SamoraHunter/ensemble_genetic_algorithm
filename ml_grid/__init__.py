d29be9ec3196d06a0df2aa8d7e90f92229705749
SamoraHunter <samorahunter@hotmail.co.uk>
2025-08-05 23:31:20 +0100

    removed erroneous import

--- ml_grid/__init__.py
+++ ml_grid/__init__.py
@@ -1,9 +1,4 @@
 print("---Ensemble GA---")
 
-
-from ml_grid.model_classes import logistic_regression_class
 from ml_grid.pipeline import data, read_in
-
-# from ml_grid.model_classes import logistic_regression_class
-#
 from ml_grid.util import param_space
